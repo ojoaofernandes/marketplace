c4fe450689266f02e671532ef78eaacc9f92361f
João Fernandes <ojoaofernandes@gmail.com>
2017-12-04 17:55:12 -0200

    Ensures that all fields are present in user form and offer form

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  validates_presence_of :title, :description, :price, :category
+
   scope :all_ordered, -> { order view_count: :desc, title: :asc }
 
   mount_uploader :photo, PhotoUploader
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :offers
+  validates_presence_of :name, :address, :phone
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
