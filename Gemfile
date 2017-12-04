43a37d416a955539a1fcdfe2f1bccffe596ba68f
João Fernandes <ojoaofernandes@gmail.com>
2017-12-04 01:28:28 -0200

    Add jQuery, Bootstrap, and change auth views to Bootstrap

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 
+gem 'jquery-rails'
+gem 'bootstrap', '~> 4.0.0.beta2.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def nav_link(description, path)
+    content_tag(:li, class: 'nav-item') do
+      link_to description, path, class: 'nav-link'
+    end
+  end
+
+  def title title
+    content_tag :div, class: 'page-header' do
+      content_tag :h2 do
+        title
+      end
+    end
+  end
+
+  def date_humanize date
+    date.strftime('%d/%m/%Y %I:%M %p')
+  end
 end
