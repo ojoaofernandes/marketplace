b5ec1d8e26424e8091cb4086016713ef2471861a
João Fernandes <ojoaofernandes@gmail.com>
2017-12-04 02:43:04 -0200

    Show offers on homepage ordered by view count and title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @offers = Offer.all_ordered
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,3 @@
 class Offer < ApplicationRecord
+  scope :all_ordered, -> { order view_count: :desc, title: :asc }
 end
